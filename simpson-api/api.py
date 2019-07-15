0122560f9107e06b06088e0042f93a26cb1348a2
davidrossouw <davidrossouw@gmail.com>
2019-07-15 09:31:44 -0400

    flask classifier working

--- simpson-api/api.py
+++ simpson-api/api.py
@@ -5,9 +5,10 @@ from flask_restful import Resource, Api, reqparse
 import numpy as np
 import cv2
 import time
-from model import SimpsonClassifier	
+from model import SimpsonClassifier
 
-import werkzeug, os
+import werkzeug
+import os
 
 # Instantiate the app
 app = Flask(__name__)
@@ -15,15 +16,14 @@ api = Api(app)
 
 UPLOAD_FOLDER = 'static/img'
 parser = reqparse.RequestParser()
-parser.add_argument('file',type=werkzeug.datastructures.FileStorage, location='files')
+parser.add_argument(
+    'file', type=werkzeug.datastructures.FileStorage, location='files')
+
 
 def read_image(file):
-
-    #read image file string data
+    '''Convert file byte object to cv2 image numpy array'''
     filestr = file.read()
-    #convert string data to numpy array
-    print(type(filestr))
-
+    # convert string data to numpy array
     nparr = np.fromstring(filestr, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
@@ -31,130 +31,56 @@ def read_image(file):
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'world1'}
 
 
 class PhotoUpload(Resource):
-    decorators=[]
+    decorators = []
 
     def post(self):
         data = parser.parse_args()
         if data['file'] == "":
             return {
-                    'data':'',
-                    'message':'No file found',
-                    'status':'error'
-                    }
+                'data': '',
+                'message': 'No file found',
+                'status': 'error'
+            }
         photo = data['file']
-        print(type(photo))
         if photo:
             filename = 'your_image.png'
-            #photo.save(os.path.join(UPLOAD_FOLDER,filename))
+            # save image to disk
+            # photo.save(os.path.join(UPLOAD_FOLDER,filename))
 
-            # read image file 
+            # read image file
             img = read_image(photo)
             img_size = 64
-            img = cv2.resize(img, (img_size, img_size)).astype('float32') / 255.
+            img = cv2.resize(img, (img_size, img_size)
+                             ).astype('float32') / 255.
             img = np.expand_dims(img, axis=0)
 
             # Instantiate model
-            model = SimpsonClassifier(weights_path='./data/weights.best.hdf5', pic_size=img_size)
-            
+            model = SimpsonClassifier(
+                weights_path='./data/weights.best.hdf5', pic_size=img_size)
+
             # Run model
-            result = model.run(img)	
-
-
+            result = model.run(img)
+            # 	return {"y_pred": result['y_pred'], "y_prob": result['y_prob']}
             return {
-                    'data':'',
-                    'message':'photo uploaded',
-                    'status':'success',
-                    'image size': img.shape
-
-                    }
+                'data': '',
+                'message': 'photo uploaded',
+                'status': 'success',
+                'image size': img.shape,
+                'result': result
+            }
         return {
-                'data':'',
-                'message':'Something when wrong',
-                'status':'error'
-                }
-
-
-# import jsonpickle
-# import numpy as np
-# import cv2
-
-
-# # route http posts to this method
-# @app.route('/api/test', methods=['POST'])
-# def test():
-#     r = request
-#     # convert string of image data to uint8
-#     nparr = np.fromstring(r.data, np.uint8)
-#     # decode image
-#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-#     # do some fancy processing here....
-
-#     # build a response dict to send back to client
-#     response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
-#                 }
-#     # encode response using jsonpickle
-#     response_pickled = jsonpickle.encode(response)
-
-#     return Response(response=response_pickled, status=200, mimetype="application/json")
-
+            'data': '',
+            'message': 'Something when wrong',
+            'status': 'error'
+        }
 
 
 api.add_resource(HelloWorld, '/')
-api.add_resource(PhotoUpload,'/upload')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.post("/items/")
-# async def create_item(item: Item):
-# 	item_dict = item.dict()
-# 	if item.tax:
-# 		price_with_tax = item.price + item.tax
-# 		item_dict.update({"price_with_tax": price_with_tax})
-# 	return item_dict
-
-
-# @app.post("/classify_image/")
-# async def classify_image(file: bytes = File(...)):
-# 	# Prepare image
-# 	img_size = 64
-# 	img = cv2.imdecode(np.fromstring(file, np.uint8), cv2.IMREAD_COLOR)
-# 	img = cv2.resize(img, (img_size, img_size)).astype('float32') / 255.
-# 	img = np.expand_dims(img, axis=0)
-	
-# 	# Instantiate model
-# 	model = SimpsonClassifier(weights_path='./data/weights.best.hdf5', pic_size=img_size)
-	
-# 	# Run model
-# 	result = model.run(img)	
-	
-# 	return {"y_pred": result['y_pred'], "y_prob": result['y_prob']}
-
-
-
-
-
-
-
-
-# Create routes
-api.add_resource(Product, '/')
+api.add_resource(PhotoUpload, '/upload')
 
 # Run the application
 if __name__ == '__main__':
